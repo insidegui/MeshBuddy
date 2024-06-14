7dbe9c69e7fd97063f7af86443c4808caa5a9af5
Gui Rambo <insidegui@gmail.com>
2024-06-14 10:41:29 -0300

    Implemented undo/redo

--- MeshBuddy/ContentView.swift
+++ MeshBuddy/ContentView.swift
@@ -1,12 +1,15 @@
 import SwiftUI
 
 struct ContentView: View {
+    @Environment(\.undoManager)
+    private var undoManager
+
     @Binding var document: MeshGradientDefinitionDocument
 
     @State private var showingConfigurationSheet = false
 
     var body: some View {
-        MeshGradientEditor(gradient: $document.definition)
+        MeshGradientEditor(gradient: undoBinding)
             .task {
                 if document.definition.width <= 0 || document.definition.height <= 0 {
                     showingConfigurationSheet = true
@@ -15,6 +18,28 @@ struct ContentView: View {
             .sheet(isPresented: $showingConfigurationSheet) {
                 DocumentConfigurationSheet(definition: $document.definition)
             }
+    }
+
+    /// This binding is sent to the editor view, so that every change performed creates a snapshot
+    /// of the current gradient definition state, enabling undo/redo by just registering the gradient state
+    /// with the undo manager as it's changed.
+    private var undoBinding: Binding<MeshGradientDefinition> {
+        Binding {
+            return document.definition
+        } set: { newValue in
+            guard let undoManager else {
+                self.document.definition = newValue
+                return
+            }
+
+            let oldValue = self.document.definition
+
+            self.document.definition = newValue
+
+            undoManager.registerUndo(withTarget: NSApplication.shared) { target in
+                self.document.definition = oldValue
+            }
+        }
     }
 }
 
